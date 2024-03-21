9406b4e7d918afbd7dff338579cea964a9a01bb5
Claudio Contardo <claudio.contardo@concordia.ca>
2024-03-20 20:20:37 -0400

    added cmake config files

---
+++ build/CMakeFiles/mis.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ccontard/git/Maximum-Independent-Set/src/Alg.cpp" "CMakeFiles/mis.dir/Alg.cpp.o" "gcc" "CMakeFiles/mis.dir/Alg.cpp.o.d"
+  "/home/ccontard/git/Maximum-Independent-Set/src/Graph.cpp" "CMakeFiles/mis.dir/Graph.cpp.o" "gcc" "CMakeFiles/mis.dir/Graph.cpp.o.d"
+  "/home/ccontard/git/Maximum-Independent-Set/src/Mis.cpp" "CMakeFiles/mis.dir/Mis.cpp.o" "gcc" "CMakeFiles/mis.dir/Mis.cpp.o.d"
+  "/home/ccontard/git/Maximum-Independent-Set/src/Reductions.cpp" "CMakeFiles/mis.dir/Reductions.cpp.o" "gcc" "CMakeFiles/mis.dir/Reductions.cpp.o.d"
+  "/home/ccontard/git/Maximum-Independent-Set/src/Util.cpp" "CMakeFiles/mis.dir/Util.cpp.o" "gcc" "CMakeFiles/mis.dir/Util.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mis.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mis.dir/Alg.cpp.o"
+  "CMakeFiles/mis.dir/Alg.cpp.o.d"
+  "CMakeFiles/mis.dir/Graph.cpp.o"
+  "CMakeFiles/mis.dir/Graph.cpp.o.d"
+  "CMakeFiles/mis.dir/Mis.cpp.o"
+  "CMakeFiles/mis.dir/Mis.cpp.o.d"
+  "CMakeFiles/mis.dir/Reductions.cpp.o"
+  "CMakeFiles/mis.dir/Reductions.cpp.o.d"
+  "CMakeFiles/mis.dir/Util.cpp.o"
+  "CMakeFiles/mis.dir/Util.cpp.o.d"
+  "libmis.pdb"
+  "libmis.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
