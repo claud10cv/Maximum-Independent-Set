d7887676ff37d5d9ead061ba999ae7d2da4a16ce
Claudio Contardo <ccontard@gmail.com>
2024-04-01 16:43:04 -0400

    Update Jlwrapper.h

--- src/Jlwrapper.h
+++ src/Jlwrapper.h
@@ -1,4 +1,4 @@
 #include "jlcxx/jlcxx.hpp"
 
-static void max_indep_set(jlcxx::ArrayRef<int> _src, jlcxx::ArrayRef<int> _dst, jlcxx::ArrayRef<int> _sol);
-JLCXX_MODULE define_julia_module(jlcxx::Module& mod);
+static void max_indep_set_xn(jlcxx::ArrayRef<int> _src, jlcxx::ArrayRef<int> _dst, jlcxx::ArrayRef<int> _sol);
+JLCXX_MODULE define_julia_module_xn(jlcxx::Module& mod);
